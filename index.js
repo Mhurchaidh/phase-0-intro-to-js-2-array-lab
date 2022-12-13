c9d0dc13fe8f4cffefc144e35c5cc8555e89863f
mhurchaidh <bmurchie1997@gmail.com>
2022-12-13 15:34:16 -0800

    commit tree 94825df149db38b971840b4eb27762fbb58ea4f6

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function(){
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const appendTheCats = [...cats, "Broom"];
+    return appendTheCats;
+}
+
+function prependCat(){
+    const prependTheCats = ["Arnold", ...cats];
+    return prependTheCats;
+}
+
+function removeLastCat(){
+    // const lastCatRemoved = cats.slice(0, -1);
+    // return lastCatRemoved;
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+    // const firstCatRemoved = cats.slice(1);
+    // return firstCatRemoved;
+    return cats.slice(1);
+}
